6abeb9947b4e4f7140ac2705560e9a443edf68ab
JuanManuel <litecorp@outlook.com>
2020-03-30 00:04:36 -0400

    Agregando Base de Datos

---
+++ HospitalJuanchos/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HospitalJuanchos.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HospitalJuanchos.Models.BDContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(HospitalJuanchos.Models.BDContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- HospitalJuanchos/Models/BDContext.cs
+++ HospitalJuanchos/Models/BDContext.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace HospitalJuanchos.Models
 {
-    public class BDContext
+    public class BDContext:DbContext
     {
+        public BDContext()
+            : base("HospJuanchos")
+        { }
+
+        public DbSet<Medicos> Medicos { get; set; }
+
+        public DbSet<Pacientes> Pacientes { get; set; }
+
+        public DbSet<Habitaciones> Habitaciones { get; set;
+        }
+        public DbSet<Citas> Citas { get; set; }
+
+        public DbSet<Ingresos> Ingresos { get; set; }
+
+        public DbSet<Altas> Altas { get; set; }
     }
 }
