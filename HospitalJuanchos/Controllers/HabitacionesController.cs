fc6a14e9a0297b555fdff36d7245142a6d834a51
JuanManuel <litecorp@outlook.com>
2020-03-30 01:49:11 -0400

    Editando Diseno de Controlador Habitaciones, Agregando los Filtros Correspondientes.

---
+++ HospitalJuanchos/Controllers/HabitacionesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HospitalJuanchos.Models;
+
+namespace HospitalJuanchos.Controllers
+{
+    public class HabitacionesController : Controller
+    {
+        private BDContext db = new BDContext();
+
+        // GET: Habitaciones
+        public ActionResult Index()
+        {
+            return View(db.Habitaciones.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(string select)
+        {
+
+            if (select == "Doble")
+            {
+                var abc = from a in db.Habitaciones
+                          where a.Tipo_Hab == Habitaciones.Tipo.Doble
+                          select a;
+                return View(abc);
+            }
+            if (select == "Suite")
+            {
+                var abc = from a in db.Habitaciones
+                          where a.Tipo_Hab == Habitaciones.Tipo.Suite
+                          select a;
+
+                return View(abc);
+            }
+            if (select == "Privada")
+            {
+                var abc = from a in db.Habitaciones
+                          where a.Tipo_Hab== Habitaciones.Tipo.Privada
+                          select a;
+
+                return View(abc);
+            }
+
+            return View(db.Habitaciones.ToList());
+
+        }
+
+        // GET: Habitaciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Habitaciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_Habitacion,Numero_Hab,Tipo_Hab,Precio")] Habitaciones habitaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Habitaciones.Add(habitaciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // POST: Habitaciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_Habitacion,Numero_Hab,Tipo_Hab,Precio")] Habitaciones habitaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(habitaciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // POST: Habitaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            db.Habitaciones.Remove(habitaciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
