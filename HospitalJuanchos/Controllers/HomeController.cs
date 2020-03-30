3f89b7f8d5d14b2733ab136456faf2cb48821036
JuanManuel <litecorp@outlook.com>
2020-03-30 00:30:48 -0400

    Editando Diseno del Controlador Home, Agregando Controlador Medicos.

--- HospitalJuanchos/Controllers/HomeController.cs
+++ HospitalJuanchos/Controllers/HomeController.cs
@@ -13,16 +13,14 @@ namespace HospitalJuanchos.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Procesos()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Mantenimiento()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
